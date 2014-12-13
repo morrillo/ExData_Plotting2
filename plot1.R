0e96e17227f188110c76b09fef35d022a44f0e8c
morrillo <gustavo.orrillo@moldeo.coop>
2014-12-13 16:27:06 -0200

    Fixed plot1

--- plot1.R
+++ plot1.R
@@ -2,4 +2,8 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 subsetNEI <- subset(NEI,year==1999|year==2002|year==2005|year==2008)
-plot(subsetNEI$year,as.numeric(as.character(subsetNEI$Emissions)),type="h")
+result <- aggregate(as.numeric(as.character(subsetNEI$Emissions)),by=list(subsetNEI$year),FUN=sum)
+data_result <- data.frame(result)
+colnames(data_result)<-c('Year','Emissions')
+barplot(data_result$Emissions,legend=data_result$Year,col=c('blue','yellow','red','green'),main="Total Emissions from PM2.5",names=c("1999","2002","2005","2008"))
+
