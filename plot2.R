d87082d32aec235d8abe379bde98340055cb8fcf
morrillo <gustavo.orrillo@moldeo.coop>
2014-12-13 16:49:09 -0200

    Added legends to plot2

--- plot2.R
+++ plot2.R
@@ -3,5 +3,4 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 subsetBaltimore <- subset(NEI,fips=="24510")
 result<-aggregate(as.numeric(subsetBaltimore$Emissions),by=list(subsetBaltimore$year),FUN=sum)
-barplot(result$x,col=c("blue","red","green","yellow"),legend=result$Group.1)
-
+barplot(result$x,col=c("blue","red","green","yellow"),legend=result$Group.1,main="Emissions from PM2.5 in Baltimore City",names=c("1999","2002","2005","2008"))
